87856f94e7359881f51a80e294600a6f308452f9
akrmSujan <sujanban223@gmail.com>
2025-08-10 16:40:29 +0600

    section title added

--- src/components/NearestExpiryFood/Nearest.jsx
+++ src/components/NearestExpiryFood/Nearest.jsx
@@ -7,7 +7,7 @@ const Nearest = () => {
     return (
         <div className="max-w-7xl mx-auto my-12 md:px-8 px-4">
             <div className="text-center mb-8">
-                <h2 className="text-4xl md:text-5xl font-extrabold dark:text-transparent text-gray-500 bg-clip-text dark:bg-gradient-to-r from-green-500 via-yellow-600 to-green-700">
+                <h2 className="text-4xl md:text-5xl font-extrabold">
                     Don’t Let Good Food Go to Waste!
                 </h2>
                 <p className="mt-3 text-gray-600 dark:text-gray-300 text-sm md:text-base max-w-xl mx-auto">
--- src/components/sectionone/ExpiredFood.jsx
+++ src/components/sectionone/ExpiredFood.jsx
@@ -7,7 +7,7 @@ const ExpiredFood = () => {
     return (
         <div className="max-w-7xl mx-auto my-12 md:px-8 px-4">
             <div className="text-center mb-8">
-                <h2 className="text-4xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-red-600 via-orange-500 to-yellow-400">
+                <h2 className="text-4xl md:text-5xl font-extrabold">
                     Oops! Some Items Have Expired
                 </h2>
                 <p className="mt-3 text-gray-600 dark:text-gray-300 text-sm md:text-base max-w-xl mx-auto">
